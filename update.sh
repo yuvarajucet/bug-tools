2b478a798cd8a92cd381e42c2d45c29ce897444f
yuvaraj (Mr_3rr0r_501) <58068070+yuvarajucet@users.noreply.github.com>
2021-01-27 11:40:22 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 echo "\033[1;32m [+] Updating Please wait..."
+sleep 1
 echo "\033[1;32m [+] Removing Old files.."
+sleep 1
 sudo rm -rf *
 echo "\033[1;32m [+] Cloning New files"
 sleep 2
