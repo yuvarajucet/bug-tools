a7e41ecf35f5721735cc50a832ca7b7a7200e32c
yuvaraj (Mr_3rr0r_501) <58068070+yuvarajucet@users.noreply.github.com>
2021-01-27 11:41:33 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,8 +4,10 @@ echo "\033[1;32m [+] Updating Please wait..."
 sleep 1
 echo "\033[1;32m [+] Removing Old files.."
 sleep 1
-sudo rm -rf *
+sudo rm -rf ../subdomain-takeover
 echo "\033[1;32m [+] Cloning New files"
 sleep 2
-git clone https://github.com/yuvarajucet/subdomain-takeover.git
+cd .. && git clone https://github.com/yuvarajucet/subdomain-takeover.git
+
 echo "\033[1;32m [+] Tool Updated success.."
+cd subdomain-takeover
